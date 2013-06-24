f08e883b8cb5cd8f2906c1526386bfa30602b2c2
Karl Monaghan <karl.t.monaghan@gmail.com>
2013-06-24 21:34:28 +0100

    Updated moderation comment to explain reasons for why comment could be moderated

--- wp-content/themes/twentytwelve-child/functions.php
+++ wp-content/themes/twentytwelve-child/functions.php
@@ -45,3 +45,76 @@ function twentytwelve_entry_meta() {
 	);
 }
 endif;
+
+if ( ! function_exists( 'twentytwelve_comment' ) ) :
+/**
+ * Template for comments and pingbacks.
+*
+* To override this walker in a child theme without modifying the comments template
+* simply create your own twentytwelve_comment(), and that function will be used instead.
+*
+* Used as a callback by wp_list_comments() for displaying the comments.
+*
+* @since Twenty Twelve 1.0
+*/
+function twentytwelve_comment( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment;
+	switch ( $comment->comment_type ) :
+	case 'pingback' :
+	case 'trackback' :
+		// Display trackbacks differently than normal comments.
+		?>
+	<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
+		<p><?php _e( 'Pingback:', 'twentytwelve' ); ?> <?php comment_author_link(); ?></p>
+	<?php
+			break;
+		default :
+		// Proceed with normal comments.
+		global $post;
+	?>
+	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+		<article id="comment-<?php comment_ID(); ?>" class="comment">
+			<header class="comment-meta comment-author vcard">
+				<?php
+					echo get_avatar( $comment, 44 );
+					printf( '<cite class="fn">%1$s %2$s</cite>',
+						get_comment_author_link(),
+						// If current post author is also comment author, make it known visually.
+						( $comment->user_id === $post->post_author ) ? '<span> ' . __( 'Post author', 'twentytwelve' ) . '</span>' : ''
+					);
+					printf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
+						esc_url( get_comment_link( $comment->comment_ID ) ),
+						get_comment_time( 'c' ),
+						/* translators: 1: date, 2: time */
+						sprintf( __( '%1$s at %2$s', 'twentytwelve' ), get_comment_date(), get_comment_time() )
+					);
+				?>
+			</header><!-- .comment-meta -->
+
+			<?php if ( '0' == $comment->comment_approved ) : ?>
+				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentytwelve' ); ?>
+				<br />Some possible reasons for this are:					
+<ul>
+<li>First time posting with this email address</li>
+<li>More than two links</li>
+<li>You've triggered our crude filter and a human needs to look at it.</li>
+<li>You're on Santa's naughty list</li>
+</ul>
+Note that we're not online 24/7 so your comment may not be approved for a little while.	
+				</p>
+			<?php endif; ?>
+
+			<section class="comment-content comment">
+				<?php comment_text(); ?>
+				<?php edit_comment_link( __( 'Edit', 'twentytwelve' ), '<p class="edit-link">', '</p>' ); ?>
+			</section><!-- .comment-content -->
+
+			<div class="reply">
+				<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'twentytwelve' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+			</div><!-- .reply -->
+		</article><!-- #comment-## -->
+	<?php
+		break;
+	endswitch; // end comment_type check
+}
+endif;
