8060a285a6b640c9987dc98828925c64ceba96fb
Karl Monaghan <karl.t.monaghan@gmail.com>
2014-04-10 10:32:30 +0100

    Adding nag links to bottom

--- wp-content/themes/twentytwelve-child/single.php
+++ wp-content/themes/twentytwelve-child/single.php
@@ -13,7 +13,12 @@ get_header(); ?>
 		<div id="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-
+				<nav class="nav-single">
+					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
+					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+				</nav><!-- .nav-single -->
+				
 				<?php get_template_part( 'content', get_post_format() ); ?>
 
 				<nav class="nav-single">
